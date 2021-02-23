faf163cb894e87b28a1ae37908df8fd1fca5b862
Dominic DeWolfe <domdewolfe@gmail.com>
2021-02-23 16:42:16 -0500

    fix error

--- setup.sh
+++ setup.sh
@@ -16,4 +16,4 @@ if ! command -v go &> /dev/null; then
   sudo apt-get install -y golang
 fi
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vimif ! command -v nvim &> /dev/null; then
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
