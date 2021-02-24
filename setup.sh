6fb2d7936151aabc85890662fc279046c0aea61a
Dominic DeWolfe <domdewolfe@gmail.com>
2021-02-23 21:18:12 -0500

    update gem and pip

--- setup.sh
+++ setup.sh
@@ -15,5 +15,12 @@ fi
 if ! command -v go &> /dev/null; then
   sudo apt-get install -y golang
 fi
+if ! command -v pip3 &> /dev/null; then
+  sudo apt-get install -y python3-pip
+fi
+pip3 install --upgrade msgpack
+sudo  npm install -g neovim
+pip3 install --upgrade  pynvim
+gem install neovim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
